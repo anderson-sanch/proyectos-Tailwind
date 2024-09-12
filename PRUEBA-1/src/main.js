7202c735c7c8793695fab20af3d54b295b4cd7f3
Anderson Sanchez <118787503+anderson-sanch@users.noreply.github.com>
2024-09-12 09:21:12 -0500

    finalizacion de la prueba, js mejorado

--- PRUEBA-1/src/main.js
+++ PRUEBA-1/src/main.js
@@ -1,52 +1,98 @@
+//Mi intento
+/* (function () {
+    /variables
+    const markAll = document.querySelector('#mark-all');
+    const contenedor = document.querySelector('.container');
+    
+    //eventos
+    eventListener();
+    function eventListener () {
+        markAll.addEventListener('click', ()=> {
+            //seleccionamos todos los no-read
+            const notReadElements = document.querySelectorAll('.not-read')
+            //eliminamos los nod-read
+            notReadElements.forEach(elemntos => {
+                elemntos.classList.remove('not-read')
+            });
+    
+            actualizarCantidad(0);
+        });
+    
+        document.addEventListener('DOMContentLoaded', () => {
+            //Seleecionamos el campo a manipular
+            const numberNotification = document.querySelector('.number-notification');
+            //obtenemos la cantidad de notificaciones
+            const notificacionCantidad = document.querySelectorAll('.not-read').length;
+            
+            //insertamos la cantidad de notificaciones
+            numberNotification.textContent = notificacionCantidad
+        })
+    
+        contenedor.addEventListener('click', (e) => {
+            if(e.target.classList.contains('card')){
+                const marca = e.target.children[1].children[0].children[3];
+                if(marca.classList.contains('not-read')){
+                    //eliminamos la marca
+                    marca.classList.remove('not-read');
+    
+                    //contamos las marcas restantes
+                    const marcasRestantes = document.querySelectorAll('.not-read').length;
+                    
+                    //actualizamos la cantidad
+                    actualizarCantidad(marcasRestantes)
+                }
+            }
+        })
+    };
+    
+    //funciones
+    function actualizarCantidad (cantidad) {
+        
+        const numberNotification = document.querySelector('.number-notification');
+    
+        numberNotification.textContent = cantidad;
+    };
+    
+})()
+ */
+
 //variables
-const markAll = document.querySelector('#mark-all');
-const contenedor = document.querySelector('.container');
+const allMarks = document.querySelector('#mark-all');
+const posts = document.querySelectorAll('.post');
+
+posts.forEach(post => {
+    post.addEventListener('click' , () => {
+        const status = post.querySelector('.status');
+        if(status){
+            status.classList.remove('not-read')
+        }
+        cantidadNotificacion();
+    })
+})
+
 
 //eventos
 eventListener();
 function eventListener () {
-    markAll.addEventListener('click', ()=> {
-        //seleccionamos todos los no-read
-        const notReadElements = document.querySelectorAll('.not-read')
-        //eliminamos los nod-read
-        notReadElements.forEach(elemntos => {
-            elemntos.classList.remove('not-read')
-        });
+    allMarks.addEventListener('click', () => {
+        const allNotRead = document.querySelectorAll('.not-read');
 
-        actualizarCantidad(0);
+        allNotRead.forEach(elemento => {
+            elemento.classList.remove('not-read')
+        });
+        cantidadNotificacion()
     });
 
-    document.addEventListener('DOMContentLoaded', () => {
-        //Seleecionamos el campo a manipular
-        const numberNotification = document.querySelector('.number-notification');
-        //obtenemos la cantidad de notificaciones
-        const notificacionCantidad = document.querySelectorAll('.not-read').length;
-        
-        //insertamos la cantidad de notificaciones
-        numberNotification.textContent = notificacionCantidad
-    })
-
-    contenedor.addEventListener('click', (e) => {
-        if(e.target.classList.contains('card')){
-            const marca = e.target.children[1].children[0].children[3];
-            if(marca.classList.contains('not-read')){
-                //eliminamos la marca
-                marca.classList.remove('not-read');
-
-                //contamos las marcas restantes
-                const marcasRestantes = document.querySelectorAll('.not-read').length;
-                
-                //actualizamos la cantidad
-                actualizarCantidad(marcasRestantes)
-            }
-        }
-    })
+    document.addEventListener('DOMContentLoaded', cantidadNotificacion);
 };
 
-//funciones
-function actualizarCantidad (cantidad) {
-    
-    const numberNotification = document.querySelector('.number-notification');
 
-    numberNotification.textContent = cantidad;
+//funciones
+
+function cantidadNotificacion () {
+    const notificacion = document.querySelector('.number-notification');
+
+    const cantidadNotificacion = document.querySelectorAll('.not-read').length;
+
+    notificacion.textContent = cantidadNotificacion
 };
